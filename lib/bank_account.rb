affce23ea4f6299c6f51a16d6086104f7b6999f2
Scott  Keplinger <skeplin17@gmail.com>
2020-05-20 00:05:09 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,7 @@
 class BankAccount
 
-  attr_accessor balance:, status:
-  attr_reader name:
+  attr_accessor :balance, :status
+  attr_reader :name
 
 
 def initialize (name, balance=1000, status="open")
