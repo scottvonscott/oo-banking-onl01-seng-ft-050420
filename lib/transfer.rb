7f7755fa24c76609df7aeab3ba55e0e40d31ada2
Scott  Keplinger <skeplin17@gmail.com>
2020-05-20 00:36:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,6 +19,8 @@ attr_accessor :sender, :receiver, :status, :amount
     @sender.balance = @sender.balance - amount
     @receiver.balance = @receiver.balance + amount
     @status = "complete"
+  elsif @sender.balance < amount
+    puts "Transaction rejected. Please check your account balance."
   else
     @status = "rejected"
   end
