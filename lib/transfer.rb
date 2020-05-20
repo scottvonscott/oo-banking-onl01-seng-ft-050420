d4cd9e68a18eda92d4c209f02720ebe2c5d96106
Scott  Keplinger <skeplin17@gmail.com>
2020-05-20 00:30:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,6 +19,7 @@ attr_accessor :sender, :receiver, :status, :amount
     @receiver.balance = @receiver.balance + amount
     @status = "complete"
     break if @sender.balance < amount
+  end
 
 
   end
