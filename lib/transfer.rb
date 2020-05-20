67704aaaf1ad5768821dff3d740bc02507b8f049
Scott  Keplinger <skeplin17@gmail.com>
2020-05-20 00:51:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ attr_accessor :sender, :receiver, :status, :amount
   end
 
   def execute_transaction
-    if @status = "pending" && @sender.valid?
+    if @status = "pending"
        @sender.balance = @sender.balance - amount
        @receiver.balance = @receiver.balance + amount
        @status = "complete"
