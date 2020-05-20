6ca5a2ed3b8be477d755a21244af9f55b3c6bc5d
Scott  Keplinger <skeplin17@gmail.com>
2020-05-20 00:22:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,8 +14,8 @@ attr_accessor :sender, :receiver, :status, :amount
   end
 
   def execute_transaction
-    @sender.@balance = @sender.@balance - amount
-    @receiver.@balance = @receiver.@balance + amount
+    @sender.balance = @sender.balance - amount
+    @receiver.balance = @receiver.balance + amount
   end
 
   def reverse_transfer
