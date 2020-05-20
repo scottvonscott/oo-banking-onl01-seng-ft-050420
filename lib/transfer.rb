25359c6ee26bd8100b5db9f6c6e7e2292ffee6ee
Scott  Keplinger <skeplin17@gmail.com>
2020-05-20 01:11:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,8 +16,8 @@ attr_accessor :sender, :receiver, :status, :amount
 
   def execute_transaction
     if @sender.balance < amount
-      "No money."
       @status = "rejected"
+      "Transaction rejected. Please check your account balance."
     elsif self.valid? && @status != "complete"
        @sender.balance = @sender.balance - amount
        @receiver.balance = @receiver.balance + amount
